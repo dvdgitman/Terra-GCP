e08643ba59a8d3cf6a3f721f6ecf7a22fc00fc56
David Gitman <wereking@gmail.com>
2021-08-11 19:47:51 +0300

    added files

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "terra-cluster"
+}
+
+variable "project" {
+  default = "terraform-k8"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
